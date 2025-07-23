e01741644a3e7229a6fc2329e11e16445a807f84
Viviane Aguiar <83668265+vivianezzt@users.noreply.github.com>
2025-07-23 14:47:00 -0300

    chore: mudando as imagens do projeto, adiciona serviços e profissionais

---
+++ src/core/profissional/Profissional.ts
@@ -0,0 +1,8 @@
+export default interface Profissional{
+    id: number;
+    nome: string;
+    descricao: string;
+    imagemURL: string;
+    avaliacao: number;
+    quantidadeAvaliacoes: number;
+}
