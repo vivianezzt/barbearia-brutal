211ff85e03a37ce700f9087f612e101604737e0f
viviane <vivianeaguiarc@outlook.com>
2025-07-26 19:59:38 -0300

    feat: adiciona a estrutura de rodapé e logo

--- frontend/src/componentes/shared/Logo.tsx
+++ frontend/src/componentes/shared/Logo.tsx
@@ -1,2 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
 
-eport default fu""
+export default function Logo() {
+    return (
+        <Link href={"/"} className="flex items-center gap-2 text-xl font-bold text-orange-400">
+            {/* You can place an <img> or text here for the logo */}
+            <Image src={"/logo.png"} alt="Logo" width={65} height={65} className="hidden sm:block" />
+            <Image src={"/logo.png"} alt="Logo" width={50} height={50} className="block sm:hidden" />
+            <div className="flex flex-col item-center">
+                <span className="text-xl sm:text-2xl">Barba</span>
+                <span>Brutal</span>
+            </div>
+        </Link>
+    );
+}
--- frontend/src/componentes/shared/Rodape.tsx
+++ frontend/src/componentes/shared/Rodape.tsx
@@ -3,51 +3,78 @@ import {
   IconBrandLinkedin,
   IconBrandTelegram,
   IconBrandThreads,
+  IconBrandWhatsapp,
   IconBrandYoutube,
 } from "@tabler/icons-react";
+import Logo from "./Logo";
 
 export default function Rodape() {
   return (
-    <footer className="container flex flex-col bg-back py-10">
-      <div className=""></div>
-      <div className="flex gap-2 text-zinc-400">
-        <a href="https://youtube.com/@vivianezzt" target="_blank" rel="noopener noreferrer">
-          <IconBrandYoutube
-            stroke={1}
-            className="cursor-pointer transition-transform duration-300 transform hover:scale-125 hover:text-orange-400"
-          />
-        </a>
-        <a href="https://instagram.com/vivianezzt" target="_blank" rel="noopener noreferrer">
-          <IconBrandInstagram
-            stroke={1}
-            className="cursor-pointer transition-transform duration-300 transform hover:scale-125 hover:text-orange-400"
-          />
-        </a>
-        <a href="https://linkedin.com/in/vivianezzt" target="_blank" rel="noopener noreferrer">
-          <IconBrandLinkedin
-            stroke={1}
-            className="cursor-pointer transition-transform duration-300 transform hover:scale-125 hover:text-orange-400"
-          />
-        </a>
-        <a href="https://www.threads.net/@vivianezzt" target="_blank" rel="noopener noreferrer">
-          <IconBrandThreads
-            stroke={1}
-            className="cursor-pointer transition-transform duration-300 transform hover:scale-125 hover:text-orange-400"
-          />
-        </a>
-        <a href="https://t.me/vivianezzt" target="_blank" rel="noopener noreferrer">
-          <IconBrandTelegram
-            stroke={1}
-            className="cursor-pointer transition-transform duration-300 transform hover:scale-125 hover:text-orange-400"
-          />
-        </a>
+    <footer className="container bg-back py-10 text-zinc-400 py-10 gap-10">
+      {/* Logo centralizado no mobile e alinhado no desktop */}
+      <div className="flex justify-between items-center flex-col md:flex-row gap-4">
+      <Logo />
+      <div className="flex flex-col gap-1 text-center md:text-left gap-2">
+        <span className="font-bold text-2xl text-orange-400">Sobre</span>
+        <span className="text-sm">Nossa Hitória</span>
+        <span className="text-sm">Política de privacidade</span>
+        <span className="text-sm">Termo de uso</span>
+      </div>
+         <div className="flex flex-col gap-1 text-center md:text-left gap-2">
+        <span className="font-bold text-2xl text-orange-400">Contato</span>
+        <span className="text-sm">suporte@barbabrutal.app</span>
+        <div className="text-sm flex items-center gap-1 cursor-pointer hover:text-green-400 mt-1 md:mt-0 hover:scale-105 transition-transform justify-center md:justify-start">
+          <IconBrandWhatsapp size={20} stroke={1} className="text-green-500" />
+          <span>Whatsapp</span>
+        </div>
+      </div>
       </div>
 
-      <div className="flex gap-1.5 text-zinc-400 mt-4">
-        <span>Feito com</span>
-        <span>🪓</span>
-        <span>em {new Date().getFullYear()}</span>
-        <span>- &copy; Todos os direitos reservados</span>
+      {/* Redes sociais e texto de direitos */}
+      <div className="flex flex-col md:flex-row justify-between items-center gap-4 md:gap-0 lg:mt-6 mt-1">
+        {/* Redes sociais */}
+        <div className="flex gap-3 justify-center md:justify-start">
+          <a href="https://youtube.com/@vivianezzt" target="_blank" rel="noopener noreferrer">
+            <IconBrandYoutube
+              stroke={1}
+              className="cursor-pointer transition-transform duration-300 transform hover:scale-125 hover:text-orange-400"
+            />
+          </a>
+          <a href="https://instagram.com/vivianezzt" target="_blank" rel="noopener noreferrer">
+            <IconBrandInstagram
+              stroke={1}
+              className="cursor-pointer transition-transform duration-300 transform hover:scale-125 hover:text-orange-400"
+            />
+          </a>
+          <a href="https://linkedin.com/in/vivianezzt" target="_blank" rel="noopener noreferrer">
+            <IconBrandLinkedin
+              stroke={1}
+              className="cursor-pointer transition-transform duration-300 transform hover:scale-125 hover:text-orange-400"
+            />
+          </a>
+          <a href="https://www.threads.net/@vivianezzt" target="_blank" rel="noopener noreferrer">
+            <IconBrandThreads
+              stroke={1}
+              className="cursor-pointer transition-transform duration-300 transform hover:scale-125 hover:text-orange-400"
+            />
+          </a>
+          <a href="https://t.me/vivianezzt" target="_blank" rel="noopener noreferrer">
+            <IconBrandTelegram
+              stroke={1}
+              className="cursor-pointer transition-transform duration-300 transform hover:scale-125 hover:text-orange-400"
+            />
+          </a>
+        </div>
+
+        {/* Texto de direitos autorais */}
+        <div className="flex flex-col items-center md:items-end text-sm">
+          <div className="flex items-center gap-1.5">
+            <span>Feito com</span>
+            <span>🪓</span>
+            <span>em {new Date().getFullYear()}</span>
+          </div>
+          <span>Todos os direitos reservados &copy;</span>
+        </div>
       </div>
     </footer>
   );
